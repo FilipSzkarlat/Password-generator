ff69a6fd3cd68168887f61e592ea449b7191ad5c
FilipSzkarlat <filip.szkarlat@gmail.com>
2024-05-22 15:40:10 +0200

    Basic password generator

---
+++ app.js
@@ -0,0 +1,113 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let password1EL = document.querySelector(".password1-el");
+let password2EL = document.querySelector(".password2-el");
+
+function generatePasswords() {
+  let password = "";
+  for (let i = 0; i < 15; i++) {
+    password += characters[randomChar()];
+  }
+  return password;
+}
+
+function randomChar() {
+  return Math.floor(Math.random() * characters.length);
+}
+
+function createPasswords() {
+  password1EL.textContent = generatePasswords();
+  password2EL.textContent = generatePasswords();
+}
